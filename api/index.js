075bba2f64f32746c003cb6788da7fe8d5820afd
Vincent Ta <tav@oregonstate.edu>
2019-06-05 12:50:59 -0700

    init index and lib folder

---
+++ api/index.js
@@ -0,0 +1,12 @@
+
+/*
+ * Top-level API router.
+ */
+
+const router = require('express').Router();
+
+router.use('/users', require('./users'));
+router.use('/courses', require('./courses'));
+router.use('/assignments', require('./assignments'));
+
+module.exports = router;
