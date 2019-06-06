7ab6780671a6ad47a0653ebf02b0c5b80613a05a
Jimwel Aguinaldo <aguinaji@oregonstate.edu>
2019-06-06 01:58:39 -0700

    Added initialization for users in mongo-init file

--- mongo-init.js
+++ mongo-init.js
@@ -8,3 +8,18 @@ db.createUser({
   pwd: "hunter2",
   roles: [ { role: "readWrite", db: "tarpaulinDB" } ]
 });
+
+db.users.insertMany([
+  {
+    "name": "Jimwel Aguinaldo",
+    "email": "jimwel.aguinaldo@oregonstate.edu",
+    "password": "$2a$08$1mCuZV6y4hdBjqqR95JlqegIIzbUR2Xz/NCn8jb0cTyIf/8LwN0le",
+    "role": "student"
+  },
+  {
+    "name": "Rob Hess",
+    "email": "rob.hess@oregonstate.edu",
+    "password": "$2a$08$1mCuZV6y4hdBjqqR95JlqegIIzbUR2Xz/NCn8jb0cTyIf/8LwN0le",
+    "role": "instructor"
+  }
+])
