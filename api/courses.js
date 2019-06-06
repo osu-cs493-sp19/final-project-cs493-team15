62cb02508662755bec601afcbc8b60d3b1953891
Jimwel Aguinaldo <aguinaji@oregonstate.edu>
2019-06-06 15:30:28 -0700

    Authenticate POST /courses endpoint where only admins can create courses.

--- api/courses.js
+++ api/courses.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
 const { validateAgainstSchema } = require('../lib/validation');
+const { requireAuthentication } = require('../lib/auth');
 
 const {
     CourseSchema,
     insertNewCourse
 } = require('../models/courses');
+const {
+  getUserById
+} = require('../models/users');
 
  /*
  * Route to get all courses.
@@ -17,24 +21,31 @@ router.get('/', async(req, res, next) => {
  /*
  * Route to create new course.
  */
-router.post('/', async(req, res, next) => {
-  if (validateAgainstSchema(req.body, CourseSchema)) {
-    try {
-      const id = await insertNewCourse(req.body);
-      res.status(201).send({
-        id: id
-      });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send({
-        error: "Error inserting course into DB.  Please try again later."
+router.post('/', requireAuthentication, async(req, res, next) => {
+  const user = await getUserById(req.user);
+  if(user.role == "admin") {
+    if (validateAgainstSchema(req.body, CourseSchema)) {
+      try {
+        const id = await insertNewCourse(req.body);
+        res.status(201).send({
+          id: id
+        });
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({
+          error: "Error inserting course into DB.  Please try again later."
+        });
+      }
+    } else {
+      res.status(400).send({
+        error: "Request body is not a valid course object."
       });
     }
   } else {
-    res.status(400).send({
-      error: "Request body is not a valid course object."
+    res.status(403).send({
+      error: "Unauthorized to access the specified resource"
     });
-}
+  }
 });
 
 /*
