7474bcc3742d95f992c9a810cbf9aca903b17072
Jimwel Aguinaldo <aguinaji@oregonstate.edu>
2019-06-05 17:44:54 -0700

    Implemented API Endpoints related to users

--- api/users.js
+++ api/users.js
@@ -5,32 +5,95 @@
  const router = require('express').Router();
 
  const { validateAgainstSchema } = require('../lib/validation');
+ const { generateAuthToken, requireAuthentication } = require('../lib/auth');
  const {
    UserSchema,
-   UserLoginSchema
+   UserLoginSchema,
+   insertNewUser,
+   validateUser,
+   getUserByEmail,
+   getUserById
 } = require('../models/users');
 
  /*
  * Route to create a new user.
  */
-router.post('/', async (req, res) => {
-
+router.post('/', async (req, res, next) => {
+  if (validateAgainstSchema(req.body, UserSchema)) {
+    try {
+     const id = await insertNewUser(req.body);
+     res.status(201).send({
+       id: id
+     });
+    } catch (err) {
+     console.error(err);
+     res.status(500).send({
+       error: "Error inserting user into DB.  Please try again later."
+     });
+    }
+  } else {
+    res.status(400).send({
+     error: "Request body is not a valid user object."
+    });
+  }
 });
 
 
 /*
-* Route to login a user.
+* Route to login as a user.
 */
-router.post('/login', async (req, res) => {
-
+router.post('/login', async (req, res, next) => {
+  if(req.body.email && req.body.password) {
+    try {
+      const authenticated = await validateUser(req.body.email, req.body.password);
+      if (authenticated) {
+        const user = await getUserByEmail(req.body.email);
+        const token = generateAuthToken(user._id);
+        res.status(200).send({
+          token: token
+        });
+      } else {
+        res.status(401).send({
+          error: "Invalid authentication credentials"
+        });
+      }
+    } catch (err) {
+      res.status(500).send({
+        error: "Error logging in.  Try again later."
+      });
+    }
+  } else {
+    res.status(400).json({
+    error: "Request body needs email and password."
+    });
+}
 });
 
 
 /*
  * Route to fetch data about a specific user.
  */
-router.get('/:id', async (req, res) => {
-
+router.get('/:id', requireAuthentication, async (req, res, next) => {
+  const user = await getUserById(req.params.id);
+  if(req.params.id === req.user) {
+    try {
+      const user = await getUserById(req.params.id);
+      if(user) {
+        res.status(200).send(user);
+      } else {
+        next();
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({
+        error: "Unable to fetch user.  Please try again later."
+      });
+    }
+  } else {
+    res.status(403).send({
+      error: "Unauthorized to access the specified resource"
+    });
+  }
 });
 
 module.exports = router;
--- models/users.js
+++ models/users.js
@@ -5,6 +5,7 @@ const { ObjectId } = require('mongodb');
 
 const { getDBReference } = require('../lib/mongo');
 const { extractValidFields } = require('../lib/validation');
+const bcrypt = require('bcryptjs');
 
 /*
  * Schema describing required/optional fields of a user object.
@@ -25,3 +26,56 @@ exports.UserSchema = UserSchema;
    password: { required: true }
  };
 exports.UserLoginSchema = UserLoginSchema;
+
+/*
+ * Executes a DB query to insert a new user into the database.  Returns
+ * a Promise that resolves to the ID of the newly-created user entry.
+ */
+async function insertNewUser(user) {
+  user = extractValidFields(user, UserSchema);
+  const db = getDBReference();
+  const collection = db.collection('users');
+
+  const passwordHash = await bcrypt.hash(user.password, 8);
+  user.password = passwordHash;
+
+  const result = await collection.insertOne(user);
+  return result.insertedId;
+}
+exports.insertNewUser = insertNewUser;
+
+/*
+* Executes a DB query to fetch information about a single specified
+* user based on its email. If no user with the
+* specified Email exists, the returned Promise will resolve to null.
+*/
+async function getUserByEmail(email) {
+  const db = getDBReference();
+  const collection = db.collection('users');
+  const results = await collection.find({ email: email }).toArray();
+  return results[0];
+}
+exports.getUserByEmail = getUserByEmail;
+
+/*
+* Executes a DB query to fetch information about a single specified
+* user based on its ID. If no user with the
+* specified ID exists, the returned Promise will resolve to null.
+*/
+async function getUserById(id) {
+  const db = getDBReference();
+  const collection = db.collection('users');
+  const results = await collection.find({ _id: new ObjectId(id) }).toArray();
+  return results[0];
+}
+exports.getUserById = getUserById;
+
+/*
+* Validates a users credentials
+*/
+async function validateUser(email, password) {
+  const user = await getUserByEmail(email);
+  const authenticated = user && await bcrypt.compare(password, user.password);
+  return authenticated;
+}
+exports.validateUser = validateUser;
